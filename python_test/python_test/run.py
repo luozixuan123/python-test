8346ebd6c4245e1feaf37af402cb6e0843f9c10e
luozixuan <luozixuan@wyundata>
2017-10-30 13:28:48 +0800

    修改

---
+++ python_test/python_test/run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# created_auth: Gekdy
+# created_at: 2017.02.24
+# updated_auth: Gekdy
+# updated_at: 2017.02.24
+
+import scrapy.cmdline
+
+if __name__ == '__main__':
+    scrapy.cmdline.execute(argv=['scrapy', 'crawl', 'test'])
---
+++ python_test/python_test/spiders/scrapy-test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+import scrapy
+from scrapy.spider import Spider
+
+
+class ScrapyTest(Spider):
+    name = "test"
+
+    def __init__(self, name=None, **kwargs):
+        Spider.__init__(self, name, **kwargs)
+
+
+    def start_requests(self):
+        a = 'http://www.cdht.gov.cn:80/zwgkjddt/121734.jhtml'
+
+        yield scrapy.Request(a)
+
+    def parse(self, response):
+        b = response.body
+
+        t1 = "".join(response.xpath("//h1//text()").extract()).strip()
+
+        print t1
